9c0dd15917abbf989f43c9cf7b498a5ce19ff7ce
John <90400991+Trippin@users.noreply.github.com>
2023-01-10 14:14:41 -0500

    Add files via upload

---
+++ NameFormat.py
@@ -0,0 +1,15 @@
+user_input = input()  
+name = user_input.split(" ")
+
+if(len(name)==3):          
+        first = name[0]     
+        middle = name[1]    
+        last = name[2]          
+        print(last + ", " + first[0] + "." + middle[0] + ".") 
+        
+elif(len(name) == 2):      
+        first = name[0]     
+        middle = name[1]      
+        print(middle + ", " + first[0] + ".")  
+        
+else:   print("The input does not have the correct form") 
